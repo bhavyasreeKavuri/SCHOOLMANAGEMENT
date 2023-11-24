32912702339569113c418f80b6a484afc4d8c8b0
bhavyasreeKavuri <111688424+bhavyasreeKavuri@users.noreply.github.com>
2023-11-24 08:38:02 +0530

    Add files via upload

---
+++ link.java
@@ -0,0 +1,22 @@
+
+Package	student-management-system
+import	StudentController.java
+import	 TeacherController.java
+import	 markscontroller.java
+
+	
+import 	Student.java
+	
+	
+import	Teacher.java
+import	 Marks.java
+     
+import	StudentRepository.java  
+import	Teacherrepository.java  
+import	MarksRepository.java
+import	StudentService.java 
+import	 TeacherServices.java 
+import	 marksService.java
+	 
+
+}
